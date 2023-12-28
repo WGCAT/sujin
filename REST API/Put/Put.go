1b98773861b9c480226c991e1396d746642ef85d
WGCAT <149450594+WGCAT@users.noreply.github.com>
2023-12-29 07:36:28 +0900

    Create Put.go

---
+++ REST API/Put/Put.go
@@ -0,0 +1,40 @@
+func TestUpdateUser(t *testing.T) { //Delete 테스트
+	assert := assert.New(t)
+
+	ts := httptest.NewServer(NewHandler())
+	defer ts.Close()
+	//Get이랑 Post는 http api에서 기본함수로 제공해주는데 delete는 없음
+	req, _ := http.NewRequest("Put", ts.URL+"/users",
+		strings.NewReader(`{"id":1, "first_name":"updated", "last_name":"updated", "email":"updated@naver.com"}`))
+	resp, err := http.DefaultClient.Do(req)
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, resp.StatusCode)
+	data, _ := ioutil.ReadAll(resp.Body)
+	assert.Contains(string(data), "No User ID:1")
+
+	resp, err = http.Post(ts.URL+"/users", "application/json",
+		strings.NewReader(`{"first_name" : "sujin", "last_name":"lee", "email":"seed9878@gmail.com"}`))
+	assert.NoError(err)
+	assert.Equal(http.StatusCreated, resp.StatusCode)
+
+	user := new(User)
+	err = json.NewDecoder(resp.Body).Decode(user)
+	assert.NoError(err)
+	assert.NotEqual(0, user.ID)
+
+	updateStr := fmt.Sprintf(`{"id":%d, "first_name":"updated", "last_name":""}`, user.ID)
+
+	req, _ = http.NewRequest("Put", ts.URL+"/users",
+		strings.NewReader(updateStr))
+	resp, err = http.DefaultClient.Do(req)
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, resp.StatusCode)
+
+	updateUser := new(User)
+	err = json.NewDecoder(resp.Body).Decode(updateUser)
+	assert.NoError(err)
+	assert.Equal(updateUser.ID, user.ID)
+	assert.Equal("updated", updateUser.FirstName)
+	assert.Equal("", updateUser.LastName)
+	assert.Equal(user.Email, updateUser.Email)
+}
