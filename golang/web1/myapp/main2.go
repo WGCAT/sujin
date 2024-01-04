9f8d9cb6754390beb43370bb515db43cb2f9d9d1
WGCAT <149450594+WGCAT@users.noreply.github.com>
2024-01-04 18:14:27 +0900

    Create main2.go

---
+++ golang/web1/myapp/main2.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+/*
+[구현해볼것]
+1. GET함수로 모든 User정보 Response
+2. GET함수로 ID입력해서 해당ID값을 가진 User정보 Response
+3. POST함수로 User정보 users map에 저장 후 Response
+4. PUT함수로 User정보 전체 수정 후 Response
+5. PUT함수로 User정보 일부 수정 후 Response
+6. DELETE함수로 ID입력해서 해당ID값을 가진 User정보 삭제 후 Response
+7. DELETE함수로 다수의 ID입력해서 해당ID값을 가진 User정보들 삭제 후 Response
+*/
+
+var users = map[string]*User{}
+
+type User struct {
+	ID        string    `json:"ID"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+func main() {
+
+	server := http.NewServeMux()
+	server.HandleFunc("/foo", fooHandler)
+	server.HandleFunc("/foo/{ID}", GetFooHandler)
+
+	err := http.ListenAndServe(":8081", server)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
+
+func fooHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		{
+			json.NewEncoder(w).Encode(users)
+		}
+	case "POST":
+		{
+			var user User
+
+			json.NewDecoder(r.Body).Decode(&user)
+			users[user.ID] = &user
+
+		}
+	case "PUT":
+		{
+			resp := "PUT디지몬"
+			w.Write([]byte(resp))
+		}
+	case "DELETE":
+		{
+			delete(users, "ID")
+		}
+	}
+}
+
+func GetFooHandler(w *http.ResponseWriter, r *http.Request) {
+	vars := server.Vars(r)
+	ID, _ := strconv.Atoi(vars{"ID"})
+	for _, user := range users {
+		if user.ID == ID {
+			json.NewEncoder(&w).Encode(user)
+			return
+		}
+	}
+
+}
